d6196983a58377c692e89d7085680e3393b421a4
EgorBarakhov <57902832+EgorBarakhov@users.noreply.github.com>
2020-05-26 09:41:47 +0300

    Udpate task 4.1 & 5.2

---
+++ ModIterator.java
@@ -0,0 +1,74 @@
+package ru.kpfu.itis.barakhov.units;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ModIterator<T> implements Iterator<T>{
+
+    private static final String ERROR_MESSAGE = "error message here";
+    private final T[] data;
+    private int cursor;
+    private final int length;
+
+    public ModIterator(T[] data, int size) {
+        this.data = data;
+        cursor = -1;
+        length = size;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cursor < length - 1;
+    }
+
+    @Override
+    public T next() {
+        if (hasNext()) {
+            return data[++cursor];
+        }
+        throw new NoSuchElementException(ERROR_MESSAGE);
+    }
+
+    public void remove() {
+        try {
+            T el = data[cursor + 1];
+            if (el == null) {
+                throw new NoSuchElementException(ERROR_MESSAGE);
+            }
+        } catch(ArrayIndexOutOfBoundsException ex) {
+            throw new NoSuchElementException(ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ModIterator)) return false;
+        ModIterator<?> that = (ModIterator<?>) o;
+        return cursor == that.cursor &&
+                length == that.length &&
+                Arrays.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(cursor, length);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ModIterator { " +
+                "data = " + Arrays.toString(data) +
+                ", cursor = " + cursor +
+                ", length = " + length +
+                " }";
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
