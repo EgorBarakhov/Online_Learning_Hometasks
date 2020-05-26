2a577e1c47fc0111aaee02f7d7c9ad76957e2030
EgorBarakhov <57902832+EgorBarakhov@users.noreply.github.com>
2020-05-26 10:38:52 +0300

    Update task 4.2 & 5.1

--- MyCollection.java
+++ MyCollection.java
@@ -1,70 +1,73 @@
+package ru.kpfu.itis.barakhov.units;
 import java.util.AbstractCollection;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
 public class MyCollection<E> extends AbstractCollection<E>{
-	
-	private static final String ERROR_MESSAGE = "error message";
-	private final int CAPACITY = 1000;
-	private E[] data;
-	private int size;
-	private MyIterator<E> myIter;
-	
-	public MyCollection() {
-		this.size = 0;
-		data = (E[]) new Object[0];
-		myIter = new MyIterator<E>(data, size);
-	}
-	
-	public MyCollection(Collection<? extends E> incol) throws IllegalArgumentException{
-		if (incol.size() > CAPACITY) throw new IllegalArgumentException(ERROR_MESSAGE);
-		this.size = 0;
-		data = (E[]) new Object[incol.size()];
-		for (E el : incol) {
-			data[size++] = el;
-		}
-		myIter = new MyIterator<E>(data, size);
-	}
-	
-	public void print() {
-		for (E el : data) {
-			System.out.println(el);
-		}
-	}
-	
-	@Override
-	public Iterator<E> iterator() {
-		return myIter;
-	}
 
-	@Override
-	public int size() {
-		return size;
-	}
+    private static final String ERROR_MESSAGE = "error message";
+    private final int CAPACITY = 1000;
+    private final E[] data;
+    private int size;
+    private final MyIterator<E> myIter;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(data);
-		result = prime * result + size;
-		return result;
-	}
+    public MyCollection() {
+        this.size = 0;
+        data = (E[]) new Object[0];
+        myIter = new MyIterator<>(data, size);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MyCollection<?> other = (MyCollection<?>) obj;
-		if (!Arrays.equals(data, other.data))
-			return false;
-		if (size != other.size)
-			return false;
-		return true;
-	}
+    public MyCollection(Collection<? extends E> incol) throws IllegalArgumentException{
+        if (incol.size() > CAPACITY) throw new IllegalArgumentException(ERROR_MESSAGE);
+        this.size = 0;
+        try {
+            data = (E[]) new Object[incol.size()];
+        } catch (ClassCastException ex) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        for (E el : incol) {
+            data[size++] = el;
+        }
+        myIter = new MyIterator<>(data, size);
+    }
+
+    public void print() {
+        for (E el : data) {
+            System.out.println(el);
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return myIter;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(data);
+        result = prime * result + size;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MyCollection<?> other = (MyCollection<?>) obj;
+        if (!Arrays.equals(data, other.data))
+            return false;
+        return size == other.size;
+    }
 }
